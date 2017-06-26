58343cf2a813ea58f94fd8828739d21dfa40cbef
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-06-26 19:43:57 +0200

    Add some styles

--- lib/src/components/radio/radio-golf/radio-golf.component.ts
+++ lib/src/components/radio/radio-golf/radio-golf.component.ts
@@ -3,28 +3,39 @@ import { ClockFactory, Clock } from '../../../shared/clock';
 import anime from 'animejs';
 
 @Component({
-  selector: 'ac-radio_golf',
-  templateUrl: './radio-golf.component.html',
-  styleUrls: ['./radio-golf.component.css']
+    selector: 'ac-radio_golf',
+    templateUrl: './radio-golf.component.html',
+    styleUrls: ['./radio-golf.component.css']
 })
 export class RadioGolfComponent {
-  private clock: Clock;
-  private readonly maxTime = 3000;
+    private clock: Clock;
+    private readonly maxTime = 3000;
+    private easing: [0, .2, 0, .95];
+    private reset: true;
 
-  constructor() {
-    this.clock = ClockFactory.create(this.maxTime);
-  }
+    constructor() {
+        this.clock = ClockFactory.create(this.maxTime);
+    }
 
-  public startCharging(): void {
-    this.clock.start();
-  }
+    public startCharging(): void {
+        this.clock.start();
+    }
 
-  public stopCharging(): void {
-    let time = this.clock.stop();
-    var cssSelector = anime({
-      targets: '.js-ball',
-      translateX: 250
-    });
-  }
+    public stopCharging(): void {
+        let time = this.clock.stop();
+        let translation = time * 100 / this.maxTime;
+        
+        anime({
+            targets: '.js-radio_golf-path',
+            translateX: `${translation}%`,
+            easing: this.easing
+        });
+
+        anime({
+            targets: '.js-radio_golf-ball',
+            translateX: `${-translation}%`,
+            easing: this.easing
+        });
+    }
 
 }
