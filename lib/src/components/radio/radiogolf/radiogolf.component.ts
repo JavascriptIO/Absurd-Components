eacfd98e7e881985793f23e2f247f8fd35f1cd77
Joan Jané <jjane@plainconcepts.com>
2017-06-28 18:51:53 +0200

    Fix horizontal scroll on radio golf. Refactor of component using ElementRef instead of document.

--- lib/src/components/radio/radiogolf/radiogolf.component.ts
+++ lib/src/components/radio/radiogolf/radiogolf.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, AfterViewChecked } from '@angular/core';
+import { Component, Input, OnInit, AfterViewChecked, ElementRef } from '@angular/core';
 import { ClockFactory, Clock } from '../../../shared/clock';
 import anime from 'animejs';
 import { Hole } from './hole.model';
@@ -11,21 +11,24 @@ import { Hole } from './hole.model';
 export class RadioGolfComponent implements OnInit, AfterViewChecked  {
   private clock: Clock;
   private readonly maxTime = 3000;
-  private easing = [0, .2, 0, .95];
+  private readonly easing = [0, .2, 0, .95];
   private animating = false;
   public holes: Array<Hole> = [];
-  private elements: any = {};
-  private selectors = {
+  private element: Element;
+  private $ball: HTMLElement;
+  private $path: HTMLElement;
+  private readonly selectors = {
     path: '.js-radiogolf-path',
     ball: '.js-radiogolf-ball',
-    hole: '.js-radiogolf-hole',
+    hole: '.js-radiogolf-hole'
   };
 
   @Input() inputs: string;
   @Input() name: string;
 
-  constructor() {
+  constructor(private elementRef: ElementRef) {
     this.clock = ClockFactory.create(this.maxTime);
+    this.element = this.elementRef.nativeElement as Element;
   }
 
   public ngOnInit(): void {
@@ -33,12 +36,12 @@ export class RadioGolfComponent implements OnInit, AfterViewChecked  {
       this.holes.push(new Hole())
     }
 
-    this.elements.path = document.querySelector(this.selectors.path);
-    this.elements.ball = document.querySelector(this.selectors.ball);
+    this.$path = this.element.querySelector(this.selectors.path) as HTMLElement;
+    this.$ball = this.element.querySelector(this.selectors.ball) as HTMLElement;
   }
 
   public ngAfterViewChecked(): void {
-    let holes = document.querySelectorAll(this.selectors.hole);
+    let holes = this.element.querySelectorAll(this.selectors.hole);
     this.holes.forEach((hole: Hole, i: number) => {
       hole.create(holes[i]);
     });
@@ -46,16 +49,16 @@ export class RadioGolfComponent implements OnInit, AfterViewChecked  {
 
   private onFinishAnimation(): void {
     this.animating = false;
-    var curTransform = new WebKitCSSMatrix(window.getComputedStyle(this.elements.path).webkitTransform);
+    var curTransform = new WebKitCSSMatrix(window.getComputedStyle(this.$path).webkitTransform);
 
-    let pathLeft = this.elements.path.offsetLeft;
+    let pathLeft = this.$path.offsetLeft;
 
-    let ballLeft = this.elements.ball.offsetLeft + curTransform.m41;
-    let ballWidth = this.elements.ball.offsetWidth;
+    let ballLeft = this.$ball.offsetLeft + curTransform.m41;
+    let ballWidth = this.$ball.offsetWidth;
 
     for (let hole of this.holes) {
-      let left = hole.base.offsetLeft - pathLeft;
-      let width = hole.base.offsetWidth;
+      let left = hole.target.offsetLeft - pathLeft;
+      let width = hole.target.offsetWidth;
       hole.active = false;
 
       if (ballLeft >= left && ballLeft <= left + width) {
@@ -73,11 +76,11 @@ export class RadioGolfComponent implements OnInit, AfterViewChecked  {
     };
 
     anime(Object.assign({}, options, {
-      targets: this.elements.path,
+      targets: this.$path
     }));
 
     anime(Object.assign({}, options, {
-      targets: this.elements.ball,
+      targets: this.$ball
     }));
   }
 
@@ -92,13 +95,12 @@ export class RadioGolfComponent implements OnInit, AfterViewChecked  {
       easing: this.easing
     };
 
-
     anime(Object.assign({}, options, {
-      targets: this.elements.path,
+      targets: this.$path,
     }));
 
     anime(Object.assign({}, options, {
-      targets: this.elements.ball,
+      targets: this.$ball,
       translateX: `${-translation}%`,
     })).finished.then(() => this.onFinishAnimation());
   }
